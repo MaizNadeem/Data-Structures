67a873ed92e290b46f2a3b391f566ddd77ab2b72
MaizNadeem <maiznadeem616@gmail.com>
2022-10-22 17:45:00 +0500

    Implemented Stack using 2 Queues.

--- QueueApps.cpp
+++ QueueApps.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 void queueApplications();
 void trafficLights();
-void priorityFunc();
 void dequeFunc();
 void stackQueue();
 
@@ -23,7 +22,7 @@ void queueApplications() {
 
     do {
         std::cout << "\nEnter the corresponding number: " << endl;
-        std::cout << "\n\t1: Traffic Lights.\n\t2: Priority Queue.\n\t3: Deque (Double Ended Queue).\n\t4: Stack Implementation with 2 queues.\n\t0: Main Menu." << endl << endl;
+        std::cout << "\n\t1: Traffic Lights.\n\t2: Deque (Double Ended Queue).\n\t3: Stack Implementation with 2 queues.\n\t0: Main Menu." << endl << endl;
 
         std::cin >> num;
 
@@ -34,12 +33,9 @@ void queueApplications() {
             trafficLights();
             break;
         case 2:
-            priorityFunc();
-            break;
-        case 3:
             dequeFunc();
             break;
-        case 4:
+        case 3:
             stackQueue();
             break;
         case 0:
@@ -48,7 +44,7 @@ void queueApplications() {
             break;
         }
 
-        if (!(num <= 4 && num >= 0))
+        if (!(num <= 3 && num >= 0))
             std::cout << "\nCorresponding number is incorrect." << endl;
     } while (num != 0);
 }
@@ -59,17 +55,9 @@ public:
     int front = -1;
     int rear = -1;
     int size = 4;
-    T* array = new T[size];
+    T array[10] = {};
 
 public:
-
-    GenericQueue(int size) {
-        this->size = size;
-    }
-
-    ~GenericQueue() {
-        delete[] array;
-    }
 
     void enqueue(T value) {
         if ((front == 0 && rear == size - 1) || (front == rear + 1)) {
@@ -121,10 +109,19 @@ public:
             cout << array[i] << endl;
         }
     }
+
+    bool empty() {
+        if (front != -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
 };
 
 void trafficLights() {
-    GenericQueue <int> q(4);
+    GenericQueue <int> q;
 
     for (int i = 0; i < q.size; i++) {
         q.enqueue(0);
@@ -163,89 +160,40 @@ void trafficLights() {
     }
 }
 
-template <class T> class PriorityQueue {
+void stackQueue() {
 
-public:
-    int front = -1;
-    int rear = -1;
-    int size = 50;
-    T* array = new T[size];
+    class StackQueue {
 
-public:
+    public:
+        GenericQueue <int> q1;
+        GenericQueue <int> q2;
+        GenericQueue <int> q;
 
-    PriorityQueue(int size) {
-        this->size = size;
-        for (int i = 0; i < size; i++)
-            array[i] = 9999;
-    }
+        void push(int value) {
+            q2.enqueue(value);
 
-    ~PriorityQueue() {
-        delete[] array;
-    }
-
-    void enqueue(T value) {
-        if ((front == 0 && rear == size - 1) || (front == rear + 1)) {
-            cout << "Queue Overflow" << endl;
-        }
-        else if (front == -1) {
-            front = 0;
-            rear = 0;
-            array[rear] = value;
-        }
-        else {
-            if (rear == size - 1) {
-                rear = 0;
+            while (!q1.empty()) {
+                q2.enqueue(q1.dequeue());
             }
-            else {
-                rear++;
-            }
-            array[rear] = value;
+
+            q = q1;
+            q1 = q2;
+            q2 = q;
         }
 
-        std::sort(array, array + size);
-    }
+        int pop() {
+            return q1.dequeue();
+        }
+    };
 
-    T dequeue() {
-        T value = 0;
-        if (front == -1) {
-            cout << "Queue Underflow" << endl;
-        }
-        else if (front == rear) {
-            value = array[front];
-            array[front] = 0;
-            front = -1;
-            rear = -1;
-        }
-        else {
-            value = array[front];
-            array[front] = 0;
-            front = (front + 1) % size;
-        }
-        return value;
-    }
-
-    void display() {
-        if (front == -1) {
-            cout << "Queue is Empty." << endl;
-        }
-        else {
-            int i;
-            for (i = front; i != rear; i = (i + 1) % size)
-                cout << array[i] << " ";
-            cout << array[i] << endl;
-        }
-    }
-};
-
-void priorityFunc() {
-    PriorityQueue <int> q(5);
-    q.enqueue(5);
-    q.enqueue(3);
-    q.enqueue(4);
-    q.enqueue(2);
-    q.enqueue(1);
-    q.dequeue();
-    q.display();
+    StackQueue s;
+    s.push(5);
+    s.push(6);
+    s.push(7);
+    cout << s.pop() << " ";
+    cout << s.pop() << " ";
+    cout << s.pop() << " ";
+    s.push(5);
 }
 
 template <class T> class Deque {
@@ -299,7 +247,7 @@ public:
         }
         else {
             if (front == 0) {
-                front = size-1;
+                front = size - 1;
             }
             else {
                 front--;
@@ -376,16 +324,5 @@ void dequeFunc() {
     q.deleteRear();
 
     q.display();
-
-}
-
-void stackQueue() {
-
-    class StackQueue {
-
-    public:
-        GenericQueue <int> q1;
-        GenericQueue <int> q2;
-    };
 
 }
