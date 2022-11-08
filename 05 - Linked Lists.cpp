14191531fdf1baa6e0874ed0ca2cc3d75f855221
MaizNadeem <maiznadeem616@gmail.com>
2022-11-08 20:33:42 +0500

    Circular List Implementation.

--- 05 - Linked Lists.cpp
+++ 05 - Linked Lists.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void linkedListApps();
 void linkedListFunc();
 void separatePrimeNumbers();
+void circularLinkedList();
 
 int main() {
 
@@ -17,7 +18,7 @@ void linkedListApps() {
 
     do {
         cout << "\nEnter the corresponding number: " << endl;
-        cout << "\n\t1: Linked List Implementation.\n\t2: Separate Prime Numbers from a list.\n\t0: Main Menu." << endl << endl;
+        cout << "\n\t1: Linked List Implementation.\n\t2: Separate Prime Numbers from a list.\n\t3: Circular Linked List.\n\t0: Main Menu." << endl << endl;
 
         std::cin >> num;
 
@@ -30,13 +31,16 @@ void linkedListApps() {
         case 2:
             separatePrimeNumbers();
             break;
+        case 3:
+            circularLinkedList();
+            break;
         case 0:
             break;
         default:
             break;
         }
 
-        if (!(num <= 2 && num >= 0))
+        if (!(num <= 3 && num >= 0))
             cout << "\nCorresponding number is incorrect." << endl;
     } while (num != 0);
 }
@@ -231,7 +235,7 @@ void linkedListFunc() {
     LinkedList<int>* list1 = new LinkedList<int>();
     LinkedList<int>* list2 = new LinkedList<int>();
 
-    list1->insertAtLast(1);                     
+    list1->insertAtLast(1);
     list1->insertAtBegin(2);
     list1->insertAtIndex(3, 2);
     list1->insertAtLast(4);                                               // 2 3 1 4
@@ -259,8 +263,8 @@ bool checkPrime(int n) {
     else if (n % 2 == 0)
         return false;
     else {
-        for (int i=3; i<n/2; i=i+2) {
-            if (n%i == 0)
+        for (int i = 3; i < n / 2; i = i + 2) {
+            if (n % i == 0)
                 return false;
         }
         return true;
@@ -274,7 +278,7 @@ void separatePrimeNumbers() {
     LinkedList<int>* list = new LinkedList<int>();
     LinkedList<int>* prime = new LinkedList<int>();
 
-    for (int i = 1; i<=100; i++) 
+    for (int i = 1; i <= 100; i++)
         list->insertAtLast(i);
 
     while (checkPrime(list->head->data)) {
@@ -309,6 +313,142 @@ void separatePrimeNumbers() {
     }
 
     list->displayList();
+    cout << endl;
     prime->displayList();
+
+}
+
+
+template <class T> class CircularLinkedList {
+
+public:
+    Node <T>* head;
+
+    CircularLinkedList() {
+        head = NULL;
+    }
+
+    void insertAtBegin(T data) {
+        Node <T>* n = new Node<T>(data);
+        Node <T>* temp = head;
+        while (temp->next != head)
+            temp = temp->next;
+        n->next = head;
+        head = n;
+        temp->next = head;
+    }
+
+    void insertAtLast(T data) {
+        Node <T>* n = new Node<T>(data);
+        if (head == NULL) {
+            head = n;
+            n->next = head;
+            return;
+        }
+        Node <T>* temp = head;
+        while (temp->next != head)
+            temp = temp->next;
+        temp->next = n;
+        n->next = head;
+    }
+
+    void deleteAtBegin() {
+        if (head == NULL) {
+            cout << "List is empty." << endl;
+            return;
+        }
+        Node <T>* temp = head;
+        while (temp->next != head)
+            temp = temp->next;
+        Node <T>* n = head;
+        head = n->next;
+        temp->next = head;
+        delete n;
+    }
+
+    void deleteAtLast() {
+        if (head == NULL) {
+            cout << "List is empty." << endl;
+            return;
+        }
+        else if (head->next == head) {
+            head = NULL;
+        }
+        else {
+            Node <T>* n = head;
+            Node <T>* temp = n;
+            while (n->next != head) {
+                temp = n;
+                n = n->next;
+            }
+            temp->next = head;
+            delete n;
+        }
+    }
+
+    void displayList() {
+        Node <T>* n = head;
+        while (n->next != head) {
+            cout << n->data << " ";
+            n = n->next;
+        }
+        cout << n->data << " " << endl;
+    }
+
+    bool searchNode(int key) {
+        Node <T>* n = head;
+        while (n->next != head) {
+            if (n->data == key)
+                return true;
+            n = n->next;
+        }
+        if (n->data == key)
+            return true;
+        return false;
+    }
+
+    static void mergeLinkedLists(CircularLinkedList<T>* list1, CircularLinkedList<T>* list2) {
+        Node <T>* n = list1->head;
+        Node <T>* p = list2->head;
+        while (n->next != list1->head)
+            n = n->next;
+        n->next = list2->head;
+        while (p->next != list2->head)
+            p = p->next;
+        p->next = list1->head;
+    }
+
+};
+
+
+void circularLinkedList() {
+
+    CircularLinkedList <int>* c1 = new CircularLinkedList<int>();
+    CircularLinkedList <int>* c2 = new CircularLinkedList<int>();
+
+    c1->insertAtLast(5);
+    c1->insertAtLast(6);
+    c1->insertAtBegin(4);
+    c1->insertAtLast(7);
+    c1->insertAtLast(8);
+    c1->insertAtLast(9);
+    c1->insertAtLast(10);
+    c1->deleteAtBegin();
+    c1->deleteAtLast();
+
+    c2->insertAtLast(5);
+    c2->insertAtLast(6);
+    c2->insertAtBegin(4);
+    c2->insertAtLast(7);
+    c2->insertAtLast(8);
+    c2->insertAtLast(9);
+    c2->insertAtLast(10);
+    c2->deleteAtBegin();
+    c2->deleteAtLast();
+
+
+    CircularLinkedList<int>::mergeLinkedLists(c1, c2);
+
+    c1->displayList();
 
 }
