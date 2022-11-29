aeb854e30312be531b571bd2060383f4fd401477
MaizNadeem <maiznadeem616@gmail.com>
2022-11-29 20:18:54 +0500

    Doubly Circular Linked List Implemented Successfully.

--- 06 - Doubly Linked Lists.cpp
+++ 06 - Doubly Linked Lists.cpp
@@ -23,11 +23,11 @@ void doublylinkedListApps() {
         system("CLS");
 
         switch (num) {
-            case 1:
-                doublylinkedListFunc();
-                break;
-            default:
-                break;
+        case 1:
+            doublylinkedListFunc();
+            break;
+        default:
+            break;
         }
 
         if (!(num <= 1 && num >= 0))
@@ -36,12 +36,12 @@ void doublylinkedListApps() {
 }
 
 class Node {
-    
+
 public:
 
     int data;
-    Node* next;
     Node* prev;
+    Node* next;
 
     Node() {}
 
@@ -52,21 +52,142 @@ public:
     }
 
     ~Node() {
-        delete next;
         delete prev;
+        delete next;
     }
 
 };
 
 class CircularDoublyList {
 
-    Node* head = new Node();
+public:
+
+    Node* head = NULL;
+
+    void insertAtFront(int data) {
+        Node* p = new Node(data);
+        if (head == NULL) {
+            p->next = p->prev = p;
+            head = p;
+        }
+        else {
+            p->prev = head->prev;
+            head->prev = p;
+            p->next = head;
+            head = p;
+        }
+    }
+
+    void insertAtLast(int data) {
+        Node* p = new Node(data);
+        if (head == NULL) {
+            p->next = p->prev = p;
+            head = p;
+        }
+        else {
+            p->prev = head->prev;
+            head->prev->next = p;
+            p->next = head;
+            head->prev = p;
+        }
+    }
+
+    void insertAtData(int data, int index) {
+        Node* p = new Node(data);
+        int flag = 0;
+        Node* q = head;
+        while (q->next != head) {
+            if (q->data == index) {
+                p->next = q->next;
+                q->next->prev = p;
+                p->prev = q;
+                q->next = p;
+                flag = 1;
+                break;
+            }
+            q = q->next;
+        }
+        if (q->data == index && flag == 0) {
+            insertAtLast(data);
+        }
+    }
+
+    void deleteAtFront() {
+        if (head == NULL)
+            cout << "List is empty." << endl;
+        else {
+            head->next->prev = head->prev;
+            head->prev->next = head->next;
+            head = head->next;
+        }
+    }
+
+    void deleteAtLast() {
+        if (head == NULL)
+            cout << "List is empty." << endl;
+        else {
+            head->prev->prev->next = head;
+            head->prev = head->prev->prev;
+        }
+    }
+
+    void deleteAtData(int index) {
+        if (head == NULL)
+            cout << "List is empty." << endl;
+        else {
+            int flag = 0;
+            Node* q = head;
+            while (q->next != head) {
+                if (q->data == index) {
+                    q->next->prev = q->prev;
+                    q->prev->next = q->next;
+                    if (q == head)
+                        head = head->next;
+                    flag = 1;
+                    break;
+                }
+                q = q->next;
+            }
+            if (q->data == index && flag == 0) {
+                deleteAtLast();
+            }
+        }
+    }
+
+    void display() {
+        if (head == NULL) {
+            cout << "List is empty." << endl;
+        }
+        else {
+            Node* p = head;
+            while (p->next != head) {
+                cout << p->data << " ";
+                p = p->next;
+            }
+            cout << p->data << endl;
+        }
+    }
 
 };
 
 
 void doublylinkedListFunc() {
 
+    CircularDoublyList* list = new CircularDoublyList();
 
+    list->insertAtFront(2);
+    list->insertAtFront(1);
+    list->insertAtLast(4);
+    list->insertAtLast(5);
+    list->insertAtLast(6);
+    list->deleteAtFront();
+    list->deleteAtLast();
+    list->insertAtData(3, 2);
+    list->insertAtData(6, 5);
+    list->deleteAtData(4);
+
+    list->display();
+
+    delete list;
 
 }
